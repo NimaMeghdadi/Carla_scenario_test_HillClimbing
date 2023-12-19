f5825d4eb8bef8f1fea53fe2f3909735c2a77384
Nima <meghdadi.nima@gmail.com>
2023-12-19 09:33:00 -0500

    fix(load model): way load model change

--- search_alg/hill_climbing.py
+++ search_alg/hill_climbing.py
@@ -6,7 +6,8 @@ import itertools
 from itertools import permutations
 import random
 import matplotlib.pyplot as plt
-
+import pickle   
+import constant
 sys.path.append('E:\GitHub\Carla_scenario_test_HillClimbing')
 import neural_network 
 
@@ -15,52 +16,50 @@ class HillClimbing:
     def __init__(self):
         pass
 
-    def optimize(self,distance_ped_starts,speed_cars,speed_peds,x=""):
-        nn = neural_network.mlpregressor.MlpRegressor()
-        # Init Random
-        if x == "": x = [random.choice(speed_cars),random.choice(distance_ped_starts),random.choice(speed_peds)]
-        y = nn.predict(x,name_model = "collision_distance_model.pickle" )
-        y1 = nn.predict(x,name_model = "collision_time_model.pickle")
-        # Init selected neighbors
-        selected_neighbors_y = [y]
-        selected_neighbors_y1 = [y1]
+    def optimize(self,distance_ped_starts,speed_cars,speed_peds,x="",no_model = ""):
+        # nn = neural_network.mlpregressor.MlpRegressor()
+        nn_distance = pickle.load(open(constant.MODEL_DIR+"collision_distance{}_model.pickle".format(no_model), "rb"))
+        nn_time = pickle.load(open(constant.MODEL_DIR+"collision_time{}_model.pickle".format(no_model), "rb"))
+        if x== "" : x = [random.choice(speed_cars),random.choice(distance_ped_starts),random.choice(speed_peds)]
+        scaler = pickle.load(open(constant.MODEL_DIR+"scaler.pickle", "rb"))
+        x_scaled = scaler.transform([x])
+        y_distance = nn_distance.predict(x_scaled)
+        y_time = nn_time.predict(x_scaled)
+        
+        selected_y_distance = [y_distance]
+        selected_y_time = [y_time]
         # start hill climbing
         while True:
             improve = False
             neighbors = self.generate_neighbor(x,speed_cars,distance_ped_starts,speed_peds)
-            
             for _ in neighbors:
                 neighbor , neighbors = self.select_random_delete(neighbors)
-                y_neighbor = nn.predict(list(neighbor),name_model = "collision_distance_model.pickle")
-                y1_neighbor = nn.predict(list(neighbor),name_model = "collision_time_model.pickle")
-                if  y_neighbor < y and y1_neighbor < y1:
+                reshaped_array = np.concatenate(neighbor, axis=0).reshape(1, -1)
+                x_scaled = scaler.transform(reshaped_array)
+                y_distance_negh = nn_distance.predict(list(x_scaled))
+                y_time_negh = nn_time.predict(list(x_scaled))
+                if  y_distance_negh < y_distance and y_time_negh < y_time:
                     x = neighbor
-                    y = y_neighbor
-                    y1=y1_neighbor
+                    y_distance = y_distance_negh
+                    y_time=y_time_negh
                     improve = True
-                    selected_neighbors_y.append(y_neighbor)
-                    selected_neighbors_y1.append(y1_neighbor)
+                    selected_y_distance.append(y_distance_negh)
+                    selected_y_time.append(y_time_negh)
 
             if not improve:
                 break
-        # Plot
-        # plt.plot(selected_neighbors_y, label = "Distance to collision")
-        # plt.plot(selected_neighbors_y1, label = "Time to collision")
-        # plt.xlabel("Iteration")
-        # plt.ylabel("Distance car stops")
-        # plt.legend() 
-        # plt.show()
-        return x,selected_neighbors_y,selected_neighbors_y1
+        return x,selected_y_distance,selected_y_time
 
     def read_csv(self,dir_csv_data='./result.csv'):
         with open(dir_csv_data, 'r') as file:
             csv_reader = csv.DictReader(file)
             data = [row for row in csv_reader]
         return data
+    
     def generate_neighbor(self,x,speed_cars,distance_ped_starts,speed_peds):
-        index_speed_car = np.where(speed_cars == x[0])[0][0]
-        index_distance_ped_start = np.where(distance_ped_starts == x[1])[0][0]
-        index_speed_ped = np.where(speed_peds == x[2])[0][0]
+        index_speed_car = np.where(speed_cars == x[0])[0]
+        index_distance_ped_start = np.where(distance_ped_starts == x[1])[0]
+        index_speed_ped = np.where(speed_peds == x[2])[0]
 
         res_speed_cars = []
         res_distance_ped_starts = []
@@ -68,12 +67,16 @@ class HillClimbing:
         
         if len(distance_ped_starts)-1 >= index_distance_ped_start+1 : res_distance_ped_starts.append(distance_ped_starts[index_distance_ped_start+1])
         if 0 <= index_distance_ped_start-1: res_distance_ped_starts.append(distance_ped_starts[index_distance_ped_start-1])
+        res_distance_ped_starts.append(distance_ped_starts[index_distance_ped_start])
         if len(speed_cars)-1 >= index_speed_car+1: res_speed_cars.append(speed_cars[index_speed_car+1]) 
         if 0 <= index_speed_car-1: res_speed_cars.append(speed_cars[index_speed_car-1]) 
+        res_speed_cars.append(speed_cars[index_speed_car]) 
         if len(speed_peds)-1 >= index_speed_ped+1: res_speed_peds.append(speed_peds[index_speed_ped+1])
         if 0 <= index_speed_ped-1: res_speed_peds.append(speed_peds[index_speed_ped-1])
+        res_speed_peds.append(speed_peds[index_speed_ped])
 
         all_res = [res_speed_cars,res_distance_ped_starts,res_speed_peds]
+        
         combinations = list(itertools.product(*all_res))
         return combinations
 
--- search_alg/random_search.py
+++ search_alg/random_search.py
@@ -1,31 +1,36 @@
 import neural_network
 import random
 import matplotlib.pyplot as plt
-
+import pickle
 import neural_network 
+import constant
 class RandomSearch():
     def __init__(self):
         pass
-    def optimize(self,distance_ped_starts,speed_cars,speed_peds,iteration = 0,x=""):
-        nn = neural_network.mlpregressor.MlpRegressor()
+    def optimize(self,distance_ped_starts,speed_cars,speed_peds,iteration = 0,x="",no_model = ""):
+        # nn = neural_network.mlpregressor.MlpRegressor()
+        nn_distance = pickle.load(open(constant.MODEL_DIR+"collision_distance{}_model.pickle".format(no_model), "rb"))
+        nn_time = pickle.load(open(constant.MODEL_DIR+"collision_time{}_model.pickle".format(no_model), "rb"))
         if x== "" : x = [random.choice(speed_cars),random.choice(distance_ped_starts),random.choice(speed_peds)]
-        y = nn.predict(x,name_model = "collision_distance_model.pickle" )
-        y1 = nn.predict(x,name_model = "collision_time_model.pickle")
+        scaler = pickle.load(open(constant.MODEL_DIR+"scaler.pickle", "rb"))
+        x_scaled = scaler.transform([x])
+        y_distance = nn_distance.predict(x_scaled)
+        y_time = nn_time.predict(x_scaled)
         
-        selected_y = [y]
-        selected_y1 = [y1]
+        selected_y_distance = [y_distance]
+        selected_y_time = [y_time]
         
         for _ in range(iteration):
-            x_rand = [random.choice(speed_cars),random.choice(distance_ped_starts),random.choice(speed_peds)]
-            y_rand = nn.predict(list(x_rand),name_model = "collision_distance_model.pickle")
-            y1_rand = nn.predict(list(x_rand),name_model = "collision_time_model.pickle")
-            if  y_rand < y and y1_rand < y1:
+            x_rand = [random.uniform(1,200),random.uniform(1,100),random.uniform(1,100)]
+            x_scaled = scaler.transform([x_rand])
+            y_distance_rand = nn_distance.predict(list(x_scaled))
+            y_time_rand = nn_time.predict(list(x_scaled))
+            if  y_distance_rand < y_distance or y_time_rand < y_time:
                 x = x_rand
-                y = y_rand
-                y1=y1_rand
-                improve = True
-            selected_y.append(y)
-            selected_y1.append(y1)
+                y_distance = y_distance_rand
+                y_time=y_time_rand
+            selected_y_distance.append(y_distance)
+            selected_y_time.append(y_time)
         
         # plt.plot(selected_y, label = "Distance to collision")
         # plt.plot(selected_y1, label = "Time to collision")
@@ -34,4 +39,4 @@ class RandomSearch():
         # plt.ylabel("Distance car stops")
         # plt.legend() 
         # plt.show()
-        return x,selected_y,selected_y1
+        return x,selected_y_distance,selected_y_time
